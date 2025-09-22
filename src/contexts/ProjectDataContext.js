6f6759527b7c37c2740a2890e64e6d98c26a3b9a
Jensen Bean <118149900+Jensen654@users.noreply.github.com>
2025-09-22 11:57:07 -0600

    Delete src directory

--- src/contexts/ProjectDataContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const ProjectDataContext = createContext();
-
-export default ProjectDataContext;
--- src/contexts/PublicDataContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const PublicDataContext = createContext();
-
-export default PublicDataContext;
